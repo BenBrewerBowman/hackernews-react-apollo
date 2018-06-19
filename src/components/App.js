992ad5205f71e414f5286c31b45892550b9b9f99
benbrewerbowman <benbrewerbowman@gmail.com>
2018-06-19 15:50:30 -0600

    Create links post mutation component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 
 class App extends React.Component {
   render() {
     return (
-      <LinkList />
+      <div>
+        <LinkList />
+        <CreateLink />
+      </div>
     );
   }
 }
