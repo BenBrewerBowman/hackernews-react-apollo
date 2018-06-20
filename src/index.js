c5aac04b73e686942a544d20d8912640863f8917
benbrewerbowman <benbrewerbowman@gmail.com>
2018-06-20 12:14:56 -0600

    Every apollo request sent with bearer token

--- src/index.js
+++ src/index.js
@@ -5,15 +5,32 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-client-preset';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { BrowserRouter } from 'react-router-dom';
+import { AUTH_TOKEN } from './constants';
 
-
+// endpoint of gql server
 const httpLink = new HttpLink({ uri: 'http://localhost:4000'});
 
+// all requests are sent with auth token if present
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  const authorizationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  });
+  return forward(operation);
+});
+
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
+// apollo client
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 });
 
